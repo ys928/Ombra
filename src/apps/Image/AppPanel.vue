cabe7c07bd8db00ba6fe7e10a149d61d4bab0e37
ys928 <it_yushi@163.com>
2024-02-17 10:44:36 +0800

    1.图片工具：添加缩略图

--- src/apps/Image/AppPanel.vue
+++ src/apps/Image/AppPanel.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, reactive, ref } from 'vue';
-import { Window, Url, Notification, Img } from '~/api';
-import { KLoading } from '~/kui'
+import { Ref, onMounted, onUnmounted, reactive, ref } from 'vue';
+import { Window, Url, Notification, Img, Dialog } from '~/api';
+import { KLoading, KIPlus } from '~/kui'
 
 let imgs_path = reactive([]) as Array<string>;
 
@@ -10,6 +10,8 @@ let uf_file_drag: UnlistenFn | undefined;
 let show_img_path = ref("");
 
 let show_loading = ref(false);
+
+const ref_thumbnail = ref() as Ref<HTMLElement>;
 
 onMounted(async () => {
     uf_file_drag = await Window.event_file_drag((files) => {
@@ -53,13 +55,43 @@ async function compress() {
     }
 }
 
+async function fun_select_pic() {
+    let ret = await Dialog.open({
+        title: '选择图片',
+        multiple: true,
+        filters: [{
+            name: '图片',
+            extensions: ['jpg', 'jpeg', 'png']
+        }]
+    }) as null | string[];
+    if (ret == null) return;
+    for (let i = 0; i < ret.length; i++) {
+        imgs_path.push(ret[i]);
+    }
+    show_img_path.value = ret[0];
+}
+
+function fun_wheel(e: WheelEvent) {
+    ref_thumbnail.value.scrollLeft += e.deltaY;
+    e.preventDefault();
+}
 
 </script>
 
 <template>
     <div class="AppPanel">
         <div class="image" ref="ref_show_img">
-            <img v-show="show_img_path.length > 0" :src="Url.convert(show_img_path)" alt="">
+            <img v-if="show_img_path.length > 0" :src="Url.convert(show_img_path)" alt="">
+            <div v-else class="select" @click="fun_select_pic">
+                <KIPlus :w="50" :h="50"></KIPlus>
+            </div>
+        </div>
+        <div class="thumbnail" ref="ref_thumbnail" @wheel="fun_wheel($event)">
+            <template v-for="item in imgs_path">
+                <div class="item" :class="{ active: item == show_img_path }" @click="show_img_path = item">
+                    <img :src="Url.convert(item)" alt="">
+                </div>
+            </template>
         </div>
         <div class="tools">
             <div class="item" @click="compress">图片压缩</div>
@@ -76,10 +108,14 @@ async function compress() {
     height: 100%;
     position: relative;
     user-select: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     .image {
         width: 100%;
-        height: 100%;
+        height: 100px;
+        flex-grow: 1;
         display: flex;
         padding: 50px 10px;
         justify-content: center;
@@ -90,6 +126,79 @@ async function compress() {
             max-width: 100%;
             max-height: 100%;
         }
+
+        .select {
+            width: 100px;
+            height: 100px;
+            border-radius: 5px;
+            border: 1px dashed #373737;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #909399;
+            cursor: pointer;
+
+            &:hover {
+                border: 1px dashed #409EFF;
+            }
+        }
+    }
+
+    .thumbnail {
+        height: 80px;
+        margin: 10px 0;
+        display: flex;
+        justify-content: center;
+        width: 80%;
+        overflow: auto;
+        flex-flow: row;
+        align-items: center;
+
+        &::-webkit-scrollbar {
+            height: 6px;
+        }
+
+        &::-webkit-scrollbar-thumb {
+            background-color: #666;
+            border-radius: 3px;
+        }
+
+        &::-webkit-scrollbar-track {
+            background-color: #181818;
+        }
+
+        .item {
+            width: 60px;
+            height: 60px;
+            background-color: #292929;
+            flex-shrink: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 5px;
+            cursor: pointer;
+            margin: 0 5px;
+            border: 2px solid #292929;
+            padding: 5px;
+
+            img {
+                max-width: 55px;
+                max-height: 55px;
+            }
+
+            &:hover {
+                border: 2px solid #8C939F;
+            }
+        }
+
+        .active {
+            border: 2px solid #2C71FA;
+
+            &:hover {
+                border: 2px solid #2C71FA;
+            }
+        }
+
     }
 
     .tools {
